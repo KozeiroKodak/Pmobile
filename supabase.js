31b6abc8411a28a45cdc4ee32f58a0ee17918b50
KozeiroKodak <brunobomreissilva15@gmail.com>
2026-09-09 11:24:50 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -43,7 +43,7 @@
 // ============================================================
 
 const SUPABASE_URL =
-    "https://rxyvllbebtgbfbqoimwe.supabase.co/rest/v1/";
+    "https://rxyvllbebtgbfbqoimwe.supabase.co";
 
 
 // ============================================================
